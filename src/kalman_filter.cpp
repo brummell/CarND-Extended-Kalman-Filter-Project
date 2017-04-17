29d1f1e45f63be1e39ed9568b90330690dd5bf8f
Doug <doug.brummell@gmail.com>
2017-04-17 01:28:03 -0400

    the long process of continuing

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -9,42 +9,42 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-    x_ = F_*x_;
-    P_ = F_*P_*F_.transpose() + Q_;
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
     auto H_t = H_.transpose();
-    auto y = z - H_*x_;
-    auto S_ = H_*P_*H_t + R_;
+    auto y = z - H_ * x_;
+    auto S_ = H_ * P_ * H_t + R_;
     auto S_i = S_.transpose();
-    auto K_ = P_*H_t*S_i;
+    auto K_ = P_ * H_t * S_i;
     MatrixXd::Identity I_(4, 4);
-    x_ = x_ + K_*y;
-    P_ = (I_ - K_*H_)*P_;
+    x_ = x_ + K_ * y;
+    P_ = (I_ - K_ * H_) * P_;
 
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    /**
+    TODO:
+      * update the state by using Extended Kalman Filter equations
+    */
+    auto H_t = H_.transpose();
+    auto y = z - H_ * x_;
+    auto S_ = H_ * P_ * H_t + R_;
+    auto S_i = S_.transpose();
+    auto K_ = P_ * H_t * S_i;
+    MatrixXd::Identity I_(4, 4);
+    x_ = x_ + K_ * y;
+    P_ = (I_ - K_ * H_) * P_;
 }
